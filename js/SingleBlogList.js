3bd6e8618fcf9f2175f5cff494bb926550c8cbfc
Ankit <ankitanand1998find@gmail.com>
2024-11-02 22:32:26 +0530

    2nd commit

--- js/SingleBlogList.js
+++ js/SingleBlogList.js
@@ -51,11 +51,9 @@ async function fetchBlogData() {
 
 // Function to redirect to singlepost.html with filename as part of the URL
 function redirectToSinglePost(filename) {
-    // Construct the SEO-friendly URL by removing the `.html` and `blogs/` parts
-    const seoFriendlyUrl = filename.replace('/blogs/', '').replace('.html', '');
-    window.location.href = `https://ananddigitalblog.vercel.app/blogs/${seoFriendlyUrl}`;
-  }
-  
+  // Redirect to singlepost.html with the filename as a query parameter
+  window.location.href = `singlepost.html?filename=${encodeURIComponent(filename)}`;
+}
 
 // Call the function to fetch and display blog data
 fetchBlogData();
